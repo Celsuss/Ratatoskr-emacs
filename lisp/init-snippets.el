5273d731a55d004a0dd2b372c337a6f41018046d
Celsuss <jenslorden@proton.me>
2026-03-01 21:19:22 +0100

    updated keybindings to insert dates

--- lisp/init-snippets.el
+++ lisp/init-snippets.el
@@ -1,6 +1,16 @@
 ;;; -*- lexical-binding: t; -*-
 ;;; init-snippets.el --- Yasnippet and templates
 
+(defun rata-insert-date-today ()
+  "Insert today's date as an active org timestamp."
+  (interactive)
+  (org-insert-time-stamp (current-time)))
+
+(defun rata-insert-date-pick ()
+  "Open org calendar picker and insert the chosen date as an active timestamp."
+  (interactive)
+  (org-time-stamp nil))
+
 (use-package yasnippet
   :after general
   :config
@@ -12,7 +22,10 @@
     :states '(normal visual)
     "i"   '(:ignore t :which-key "insert")
     "is"  '(yas-insert-snippet :which-key "snippet")
-    "in"  '(yas-new-snippet :which-key "new snippet")))
+    "in"  '(yas-new-snippet :which-key "new snippet")
+    "id"  '(:ignore t :which-key "date")
+    "idt" '(rata-insert-date-today :which-key "today")
+    "idd" '(rata-insert-date-pick  :which-key "pick date")))
 
 (use-package yasnippet-snippets
   :after yasnippet)
