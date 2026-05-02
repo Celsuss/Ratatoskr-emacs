c007fd63b2b04571a5fcf3e0081f03d8cfc48a41
Celsuss <jenslorden@proton.me>
2026-05-02 19:35:10 +0200

    Updated khoj to read my org-roam second brain

--- lisp/init-khoj.el
+++ lisp/init-khoj.el
@@ -7,6 +7,7 @@
   (khoj-server-url "http://khoj.homelab.local")
   (khoj-server-is-local t)
   (khoj-results-count 8)
+  (khoj-index-directories (list (expand-file-name "~/workspace/second-brain/org-roam/")))
   :config
   (rata-leader
     :states '(normal visual)
@@ -15,6 +16,7 @@
     "aikc"  '(khoj--chat                        :which-key "chat")
     "aikn"  '(khoj--new-conversation-session    :which-key "new conversation")
     "aiko"  '(khoj--open-conversation-session   :which-key "open conversation")
-    "aikd"  '(khoj--delete-conversation-session :which-key "delete conversation")))
+    "aikd"  '(khoj--delete-conversation-session :which-key "delete conversation")
+    "aiku"  '(khoj--server-index-files          :which-key "update index")))
 
 (provide 'init-khoj)
